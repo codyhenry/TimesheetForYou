4017089f1c96036ab50703e68df2586c798bf11d
Cody Henry <60040485+codyhenry@users.noreply.github.com>
2026-08-18 10:28:22 -0400

    Add dashboard user management serializers

--- timesheet_project/accounts/serializers.py
+++ timesheet_project/accounts/serializers.py
@@ -4,9 +4,25 @@ from .models import User
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
+    can_access_dashboard = serializers.BooleanField(read_only=True)
+    can_access_django_admin = serializers.BooleanField(read_only=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "first_name", "last_name", "email", "phone", "role", "is_staff"]
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "role",
+            "is_active",
+            "is_staff",
+            "force_password_change",
+            "can_access_dashboard",
+            "can_access_django_admin",
+        ]
 
 
 class NannySerializer(serializers.ModelSerializer):
@@ -24,6 +40,7 @@ class NannySerializer(serializers.ModelSerializer):
             "phone",
             "role",
             "is_active",
+            "force_password_change",
         ]
         read_only_fields = ["role"]
 
@@ -33,8 +50,10 @@ class NannySerializer(serializers.ModelSerializer):
         user.role = User.Role.NANNY
         if password:
             user.set_password(password)
+            user.force_password_change = validated_data.get("force_password_change", True)
         else:
             user.set_unusable_password()
+            user.force_password_change = True
         user.save()
         return user
 
@@ -45,5 +64,62 @@ class NannySerializer(serializers.ModelSerializer):
         instance.role = User.Role.NANNY
         if password:
             instance.set_password(password)
+            instance.force_password_change = True
+        instance.save()
+        return instance
+
+
+class DashboardUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, allow_blank=False)
+    can_access_dashboard = serializers.BooleanField(read_only=True)
+    can_access_django_admin = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "role",
+            "is_active",
+            "is_staff",
+            "force_password_change",
+            "can_access_dashboard",
+            "can_access_django_admin",
+        ]
+        read_only_fields = ["is_staff"]
+
+    def validate_role(self, value):
+        if value not in {User.Role.OFFICE, User.Role.ADMIN}:
+            raise serializers.ValidationError("Dashboard users must have the office or admin role.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User(**validated_data)
+        user.is_staff = False
+        if password:
+            user.set_password(password)
+            user.force_password_change = validated_data.get("force_password_change", True)
+        else:
+            user.set_unusable_password()
+            user.force_password_change = True
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            if attr == "is_staff":
+                continue
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+            instance.force_password_change = True
+        instance.is_staff = False if not instance.is_superuser else instance.is_staff
         instance.save()
         return instance
