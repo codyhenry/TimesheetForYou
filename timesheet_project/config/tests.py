f529594246dad8986c0b846a9b5933d712407bf0
Cody Henry <60040485+codyhenry@users.noreply.github.com>
2026-08-18 23:23:51 -0400

    Add media storage settings tests

--- timesheet_project/config/tests.py
+++ timesheet_project/config/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase, override_settings
+from django.conf import settings
+from django.test import SimpleTestCase, TestCase, override_settings
 from django.urls import reverse
 
 
@@ -24,3 +25,46 @@ class RootAndErrorRoutingTests(TestCase):
         self.assertEqual(response.status_code, 404)
         self.assertEqual(response["Content-Type"], "application/json")
         self.assertEqual(response.json(), {"detail": "Not found."})
+
+
+class MediaStorageSettingsTests(SimpleTestCase):
+    def test_filesystem_storage_is_default_for_local_and_test_runs(self):
+        self.assertFalse(settings.USE_S3)
+        self.assertEqual(settings.MEDIA_URL, "/media/")
+        self.assertEqual(
+            settings.STORAGES["default"]["BACKEND"],
+            "django.core.files.storage.FileSystemStorage",
+        )
+
+    @override_settings(
+        USE_S3=True,
+        AWS_STORAGE_BUCKET_NAME="example-media-bucket",
+        AWS_S3_REGION_NAME="us-east-2",
+        AWS_LOCATION="private-media",
+        STORAGES={
+            "default": {
+                "BACKEND": "storages.backends.s3.S3Storage",
+                "OPTIONS": {
+                    "bucket_name": "example-media-bucket",
+                    "region_name": "us-east-2",
+                    "location": "private-media",
+                    "querystring_auth": True,
+                    "querystring_expire": 3600,
+                    "file_overwrite": False,
+                },
+            },
+            "staticfiles": {
+                "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            },
+        },
+    )
+    def test_s3_storage_settings_are_private_and_non_overwriting(self):
+        storage_options = settings.STORAGES["default"]["OPTIONS"]
+
+        self.assertTrue(settings.USE_S3)
+        self.assertEqual(settings.STORAGES["default"]["BACKEND"], "storages.backends.s3.S3Storage")
+        self.assertEqual(storage_options["bucket_name"], "example-media-bucket")
+        self.assertEqual(storage_options["location"], "private-media")
+        self.assertTrue(storage_options["querystring_auth"])
+        self.assertEqual(storage_options["querystring_expire"], 3600)
+        self.assertFalse(storage_options["file_overwrite"])
