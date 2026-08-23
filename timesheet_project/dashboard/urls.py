b1eb47507fc0aa1d0dfd026642dc221d48155058
Cody Henry <60040485+codyhenry@users.noreply.github.com>
2026-08-22 21:50:45 -0400

    Route dashboard password setup page

--- timesheet_project/dashboard/urls.py
+++ timesheet_project/dashboard/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("password-setup/", views.password_setup, name="dashboard-password-setup"),
     path("", views.index, name="dashboard-index"),
     path("timesheets/<int:timesheet_id>/", views.index, name="dashboard-detail"),
     path("timesheets/<int:timesheet_id>/notes/", views.update_notes, name="dashboard-notes"),
